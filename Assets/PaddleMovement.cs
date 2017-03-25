2a6c21946c6e06e4aff8269e8ca5353225f78432
Mark Williams <markzawill@gmail.com>
2017-03-25 12:00:00 -0500

    Used c# to create paddle movement.  created the speed and velocity of the paddles and then had them move only on the y axis.  Used fixed update instead of update due to wanting this updated at a fixed interval

---
+++ Assets/PaddleMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour {
+
+    public float speed = 30;
+    public string axis = "Vertical";
+    private void FixedUpdate()
+    {
+        float y = Input.GetAxisRaw("Vertical");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, y) * speed; 
+    }
+
+}
