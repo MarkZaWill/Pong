7909fb2c3210593de95bbb060719d7a4fdf46723
Mark Williams <markzawill@gmail.com>
2017-03-25 13:05:45 -0500

    Changed ball behavior so it hits at different angles depending on where it strikes the paddle.  MVP hit.  Needs ability to keep score and to win.  Then add AI to play against

---
+++ Assets/Ball.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public float speed = 30;
+  
+    void Start()
+    {
+        //this is is initial velocity of the ball
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+  public float hitFactor(Vector2 ballPosition, Vector2 PaddlePosition, float PaddleHeight)
+    {
+        return (ballPosition.y - PaddlePosition.y) / PaddleHeight;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.name =="LeftRacket")
+        {
+            float y = hitFactor(transform.position,
+                                collision.transform.position,
+                                collision.collider.bounds.size.y);
+            Vector2 direction = new Vector2(1, y).normalized;
+
+            GetComponent<Rigidbody2D>().velocity = direction * speed;
+        }
+       
+        if( collision.gameObject.name == "RightRacket")
+        {
+
+            float y = hitFactor(transform.position,
+                                collision.transform.position,
+                                collision.collider.bounds.size.y);
+
+            Vector2 direction = new Vector2(-1, y).normalized;
+
+            GetComponent<Rigidbody2D>().velocity = direction * speed;
+        }
+    }
+}
