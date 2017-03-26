44b3a27b1097c5f67f09fd855eb314811dacc95e
Mark Williams <markzawill@gmail.com>
2017-03-26 12:34:36 -0500

    Figured out what not to do.  Didn't rely enough on coding skills, relied heavily on Unity to give me what I wanted.  Have to burn down/refactor to explicitly tell the game exactly what I want

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
     public float speed = 30;
-  
+    GameObject ball;
     void Start()
     {
         //this is is initial velocity of the ball
         GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+        ball = GameObject.FindGameObjectWithTag("Ball");
     }
   public float hitFactor(Vector2 ballPosition, Vector2 PaddlePosition, float PaddleHeight)
     {
@@ -16,7 +17,11 @@ public class Ball : MonoBehaviour {
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
+        if(collision.gameObject.name == "RightWall")
+        {
+            Destroy(ball, 0.5f);
+            
+        }
         if (collision.gameObject.name =="LeftRacket")
         {
             float y = hitFactor(transform.position,
