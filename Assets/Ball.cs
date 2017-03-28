7f13585b0cc980611d6a1c37153edf7acbdeb2c4
Mark Williams <markzawill@gmail.com>
2017-03-28 15:01:07 -0500

    made the score increment

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -1,29 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour {
     public float speed = 30;
-    GameObject ball;
+    private Rigidbody2D Rigid;
+    public Vector2 right = new Vector2(20.0f, -15.0f);
+    public Vector2 left = new Vector2(-20.0f, -15.0f);
+    public Vector2 v2;
+//    void BallMove() { 
+//        float random = Random.Range(0.0f, 2.0f);
+//            if(random< 1.0f)
+//            {
+//                Rigid.AddForce(right);
+//            }
+//            else
+//            {
+//                Rigid.AddForce(left);
+//            }
+//}
     void Start()
     {
         //this is is initial velocity of the ball
         GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
-        ball = GameObject.FindGameObjectWithTag("Ball");
+       Transform ball = GameObject.FindGameObjectWithTag("Ball").transform;
     }
   public float hitFactor(Vector2 ballPosition, Vector2 PaddlePosition, float PaddleHeight)
     {
         return (ballPosition.y - PaddlePosition.y) / PaddleHeight;
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    public void ResetBall()
     {
-        if(collision.gameObject.name == "RightWall")
-        {
-            Destroy(ball, 0.5f);
-            
-        }
+        v2.y = 0;
+        v2.x = 0;
+        
+
+    }
+   void RestartGame()
+    {
+        ResetBall();
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
         if (collision.gameObject.name =="LeftRacket")
         {
+            Console.WriteLine(collision.gameObject.name);
             float y = hitFactor(transform.position,
                                 collision.transform.position,
                                 collision.collider.bounds.size.y);
@@ -42,6 +66,20 @@ public class Ball : MonoBehaviour {
             Vector2 direction = new Vector2(-1, y).normalized;
 
             GetComponent<Rigidbody2D>().velocity = direction * speed;
+        }
+
+        if( collision.gameObject.name == "RightWall")
+        {
+            
+            GameBoard.Score(collision.gameObject.name);
+            RestartGame();
+        }
+
+        if( collision.gameObject.name == "LeftWall")
+        {
+           
+            GameBoard.Score(collision.gameObject.name);
+            RestartGame();
         }
     }
 }
---
+++ Assets/GameBoard.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoard : MonoBehaviour {
+
+    public static int Player1Score = 0;
+    public static int Player2Score = 0;
+
+    public BoxCollider2D TopWall;
+    public BoxCollider2D BottomWall;
+    public BoxCollider2D LeftWall;
+    public BoxCollider2D RightWall;
+
+    public GUISkin layout;
+
+    Transform BallTransform;
+    private void Start()
+    {
+        BallTransform = GameObject.FindGameObjectWithTag("Ball").transform;
+    }
+
+    public static void Score (string Wall)
+    {
+        Console.WriteLine(Wall);
+        if (Wall == "RightWall")
+        {
+            Player1Score++;
+        }
+        else if (Wall == "LeftWall")
+        {
+            Player2Score++;
+        }
+    }
+
+   void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width / 2 - 150 - 12, 20, 100, 100), "" + Player1Score);
+        GUI.Label(new Rect(Screen.width / 2 + 150 + 12, 20, 100, 100), "" + Player2Score);
+
+        if (Player1Score == 10)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER ONE WINS");
+            BallTransform.gameObject.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
+        }
+        else if (Player2Score == 10)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER TWO WINS");
+            BallTransform.gameObject.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
+        }
+    }
+}
+
