b1f7a22fa11d2a4a7537b3bf03611b31f96236ea
Mark Williams <markzawill@gmail.com>
2017-03-29 08:27:58 -0500

    added functionality for AI and menu.  Have to hide menu and enable the AI through a start menu in next update

---
+++ Assets/Menu.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GUISkin guiSkin;
+    public Texture2D background, LOGO;
+    public bool DragWindow = false;
+    public string levelToLoadWhenClickedPlay = "";
+    public string[] AboutTextLines = new string[0];
+    
+
+    private string clicked = "", MessageDisplayOnAbout = "About \n ";
+    private Rect WindowRect = new Rect((Screen.width / 2) - 100, Screen.height / 2, 200, 200);
+    private float volume = 1.0f;
+
+    private void Start()
+    {
+        for (int x = 0; x < AboutTextLines.Length; x++)
+        {
+            MessageDisplayOnAbout += AboutTextLines[x] + " \n ";
+        }
+        MessageDisplayOnAbout += "Press Esc To Go Back";
+    }
+
+    private void OnGUI()
+    {
+        if (background != null)
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), background);
+        if (LOGO != null && clicked != "about")
+            GUI.DrawTexture(new Rect((Screen.width / 2) - 100, 30, 200, 200), LOGO);
+
+        GUI.skin = guiSkin;
+        if (clicked == "")
+        {
+            WindowRect = GUI.Window(0, WindowRect, menuFunc, "Main Menu");
+        }
+        else if (clicked == "options")
+        {
+            WindowRect = GUI.Window(1, WindowRect, optionsFunc, "Options");
+        }
+        else if (clicked == "about")
+        {
+            GUI.Box(new Rect(0, 0, Screen.width, Screen.height), MessageDisplayOnAbout);
+        }
+        else if(clicked == "Player")
+        {
+            GUILayout.Box("One Player");
+            GUILayout.Box("Two Players");
+
+            if (clicked == "One Player")
+            {
+                PaddleMovement.Player1.SetActive(true);
+                PaddleMovement.Player2.SetActive(false);
+            }
+            else if (clicked == "Two Players")
+            {
+                PaddleMovement.Player1.SetActive(true);
+                PaddleMovement.Player2.SetActive(true);
+            }
+        }
+    }
+
+    private void optionsFunc(int id)
+    {
+        if (GUILayout.Button("Player"))
+        {
+            clicked = "Player";
+        }
+        GUILayout.Box("Volume");
+        volume = GUILayout.HorizontalSlider(volume, 0.0f, 1.0f);
+        AudioListener.volume = volume;
+        if (GUILayout.Button("Back"))
+        {
+            clicked = "";
+        }
+        if (DragWindow)
+            GUI.DragWindow(new Rect(0, 0, Screen.width, Screen.height));
+    }
+
+    private void menuFunc(int id)
+    {
+        //buttons 
+        if (GUILayout.Button("Play Game"))
+        {
+            //play game is clicked
+            SceneManager.LoadScene("Ping");
+            
+        }
+        if (GUILayout.Button("Options"))
+        {
+            clicked = "options";
+        }
+        if (GUILayout.Button("About"))
+        {
+            clicked = "about";
+        }
+        if (GUILayout.Button("Quit Game"))
+        {
+            Application.Quit();
+        }
+        if (DragWindow)
+            GUI.DragWindow(new Rect(0, 0, Screen.width, Screen.height));
+    }
+
+    private void Update()
+    {
+        if (clicked == "about" && Input.GetKey(KeyCode.Escape))
+            clicked = "";
+    }
+}
--- Assets/PaddleMovement.cs
+++ Assets/PaddleMovement.cs
@@ -3,13 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PaddleMovement : MonoBehaviour {
-
+    public static GameObject Player1;
+    public static GameObject Player2;
+    public GameObject LeftRacket;
+    public GameObject RightRacket;
     public float speed = 30;
     public string axis = "Vertical";
-    private void FixedUpdate()
-    {
-        float y = Input.GetAxisRaw(axis);
-        GetComponent<Rigidbody2D>().velocity = new Vector2(0, y) * speed; 
-    }
+    Rigidbody2D rigidball;
 
-}
+    public void Start()
+    {
+    Transform ball = GameObject.FindGameObjectWithTag("Ball").transform;
+    Rigidbody2D rigidball = ball.GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+
+
+            float y = Input.GetAxisRaw(axis);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, y) * speed;
+
+
+             GameAI();
+
+    }
+    public void GameAI()
+    {
+
+        if (rigidball.velocity.x < 0)
+        {
+            if(rigidball.position.y < this.transform.position.y)
+            {
+                transform.Translate(Vector2.down * speed);
+            }
+            if(rigidball.position.y > this.transform.position.y)
+            {
+                transform.Translate(Vector2.up * speed);
+            }
+
+        }
+    }       
+            
+
+        }
